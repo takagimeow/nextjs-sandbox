b0a8c6587f6956167be4322ef094a8251a751a40
Keisuke Takagi <mit@takagi.app>
2023-04-18 14:35:27 +0900

    feat: add GlobalStyles

---
+++ src/components/functional/GlobalStyles.tsx
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  html,
+  body,
+  textarea {
+    padding: 0,
+    margin: 0,
+    font-family: -apple-ssytem, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+  * {
+    box-sizing: border-box;
+  }
+  a {
+    cursor: pointer;
+    text-decoration: none;
+    transition: .25s;
+    color: #000;
+  }
+
+  ol, ul {
+    list-style: none;
+  }
+`
