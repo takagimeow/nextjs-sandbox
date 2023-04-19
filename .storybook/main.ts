ce62665e2964e774dd2d17f57642d2649649a6cc
Keisuke Takagi <mit@takagi.app>
2023-04-19 12:23:28 +0900

    chore: update storybook config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,7 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
+import path from "path";
+
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -13,5 +16,22 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
+  staticDirs: ["public"],
+  babel: async options => ({
+    ...options,
+    plugins: [
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-private-methods',
+      '@babel/plugin-proposal-private-property-in-object',
+    ],
+  }),
+  webpackFinal: async (config) => {
+    config!.resolve!.plugins = [
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, '../tsconfig.json')
+      }),
+    ]
+    return config;
+  }
 };
 export default config;
